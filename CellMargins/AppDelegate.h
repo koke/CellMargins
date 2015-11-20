f1e4f8e1b99c27c23dbdf885b244d9829f0cbaad
aerych <aerychj@gmail.com>
2015-11-20 09:25:08 -0600

    Initial Commit

---
+++ CellMargins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CellMargins
+//
+//  Created by aerych on 11/20/15.
+//  Copyright © 2015 aerych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CellMargins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CellMargins
+//
+//  Created by aerych on 11/20/15.
+//  Copyright © 2015 aerych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
